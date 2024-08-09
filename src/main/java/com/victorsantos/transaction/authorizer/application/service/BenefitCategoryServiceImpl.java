bd7cb4e5db9fe209fbd6f6456e4dab40af87e0cd
Victor Santos Batista <victorsantos.professional@gmail.com>
2024-08-09 20:10:21 -0300

    L1: Selection of benefit category by mcc

---
+++ src/main/java/com/victorsantos/transaction/authorizer/application/service/BenefitCategoryServiceImpl.java
@@ -0,0 +1,20 @@
+package com.victorsantos.transaction.authorizer.application.service;
+
+import com.victorsantos.transaction.authorizer.domain.enums.BenefitCategory;
+import java.util.Map;
+import org.springframework.stereotype.Service;
+
+@Service
+class BenefitCategoryServiceImpl implements BenefitCategoryService {
+
+    static final Map<String, BenefitCategory> mccToCategoryMap = Map.of(
+            "5411", BenefitCategory.FOOD,
+            "5412", BenefitCategory.FOOD,
+            "5811", BenefitCategory.MEAL,
+            "5812", BenefitCategory.MEAL);
+
+    @Override
+    public BenefitCategory findByMcc(String mcc) {
+        return mccToCategoryMap.getOrDefault(mcc, BenefitCategory.CASH);
+    }
+}
